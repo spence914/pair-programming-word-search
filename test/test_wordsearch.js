bf1a63cfcef8e8a4684048ccff55387385e1f1d2
Spencer Lewis <spence.914@gmail.com>
2023-11-17 17:05:18 -0800

    implement wordsearch function, including stretch goals

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,103 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards and vertical", function() {
+    const result = wordSearch([
+      ['D', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['E', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['F', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['S', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['E', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['F', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'F', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'L', 'I'],
+      ['S', 'D', 'C', 'A', 'K', 'U', 'A', 'D'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally from top right", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'S'], // right most 'S' is at arr[0][7]
+      ['L', 'R', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['E', 'F', 'F', 'F', 'Q', 'I', 'A', 'L'],
+      ['F', 'M', 'J', 'N', 'N', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'F', 'F', 'E', 'R', 'L'],
+      ['I', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'L', 'T', 'W', 'A', 'P', 'L', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'D'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally starting from the far right column", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'S'],
+      ['L', 'R', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['E', 'F', 'F', 'F', 'Q', 'I', 'A', 'L'],
+      ['F', 'M', 'J', 'N', 'N', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'F', 'F', 'E', 'R', 'L'],
+      ['I', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'L', 'T', 'W', 'A', 'P', 'L', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'D'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'GREAAK');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,151 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Collaborated on by @spence914, @Faelixx, @aeedwko
 
-module.exports = wordSearch
+const transpose = (matrix) => {
+  const result = [];
+  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        result.push([matrix[i][j]]);
+      } else {
+        result[j].push(matrix[i][j]);
+      }
+    }
+  }
+  return result;
+};
+
+const makeDiagArr = function(letters) {
+  let diagonalArr = [];
+  for (let i = 0; i < letters.length; i++) {
+    let diagonalStr = "";
+    let x = i;
+    let y = 0;
+    while (x < letters.length) {
+      if (letters[x][y]) {
+        diagonalStr += letters[x][y];
+      }
+      x++;
+      y++;
+    }
+
+    diagonalArr.push(diagonalStr);
+  }
+    
+  for (let j = 1; j < letters[0].length; j++) {
+    let diagonalStr = "";
+    let x = 0;
+    let y = j;
+    // start building the diagonal string
+    while (y < letters[0].length) {
+      if (letters[x][y]) {
+        diagonalStr += letters[x][y];
+      }
+      x++;
+      y++;
+    }
+
+    diagonalArr.push(diagonalStr);
+  }
+  return diagonalArr;
+};
+// starting from TOP RIGHT
+const makeDiagArrReverse = function(letters) {
+  let diagonalArr = [];
+  for (let i = 0; i < letters.length; i++) {
+    let diagonalStr = "";
+    let x = i; // 9
+    let y = letters[0].length - 1; // 8
+    while (x <= letters.length - 1) {
+      if (letters[x][y]) {
+        diagonalStr += letters[x][y];
+      }
+      x++;
+      y--;
+    }
+
+    diagonalArr.push(diagonalStr);
+  }
+    
+  for (let j = 1; j < letters.length; j++) {
+    let diagonalStr = "";
+    let x = letters.length - 1;
+    let y = j;
+    // start building the diagonal string
+    while (y < letters.length) {
+      if (letters[x][y]) {
+        diagonalStr += letters[x][y];
+      }
+      x--;
+      y++;
+    }
+
+    diagonalArr.push(diagonalStr);
+  }
+  return diagonalArr;
+};
+
+const wordSearch = (letters, word) => {
+  const backwardsWord = word.split('').reverse().join('');
+
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word) || l.includes(backwardsWord)) {
+      return true;
+    }
+  }
+
+  const verticalArray = transpose(letters);
+  const verticalJoin = verticalArray.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word) || l.includes(backwardsWord)) {
+      return true;
+    }
+  }
+
+
+  let diagonalArr = makeDiagArr(letters);
+  for (let d of diagonalArr) {
+    if (d.includes(word)) {
+      return true;
+    }
+  }
+
+
+  for (let dR of diagonalArr) {
+    if (dR.split("").reverse().join("").includes(word)) {
+      return true;
+    }
+  }
+
+  let diagArrReverse =  makeDiagArrReverse(letters);
+  for (let d of diagArrReverse) {
+    if (d.includes(word)) {
+      return true;
+    }
+  }
+
+  for (let dR of diagArrReverse) {
+    if (dR.split("").reverse().join("").includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const arr = [
+  ['W', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'], // W is at arr[0][0], at each iteration, call diagonal(arr) we can check if arr[i+1][j+1] !== undefined
+  ['S', 'O', 'F', 'N', 'F', 'E', 'L', 'D'], // S is at arr[1][0]
+  ['Y', 'F', 'R', 'A', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'D', 'D', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'], // E is at arr[8][0]
+];
+
+wordSearch(arr, "WORD");
+module.exports = wordSearch;
